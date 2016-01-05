8b106e135db0216ac21fbb808a8fbd408d6981b4
josellausas <jose@josellausas.com>
2016-01-05 00:23:36 -0600

    OK is good. Some basic sprite drawing.

--- main.lua
+++ main.lua
@@ -7,6 +7,71 @@ Todos los derechos reservadas.
 
 ]]
 
-function love.draw()
-	love.graphics.print("Hello World", 400, 400)
+local imgx = 100
+local imgy = 100
+
+function love.load()
+	love.graphics.setNewFont(12)
+	love.graphics.setBackgroundColor(255,255,255)
+
+	-- Load the images here
+	blueTile = love.graphics.newImage("art/tileBlue_04.png")
+	redTile = love.graphics.newImage("art/tileRed_04.png")
 end
+
+function love.update(dt)
+	if love.keyboard.isDown("up") then
+		num = num + 100 * dt -- 100 hz
+	end
+end
+
+function love.draw()
+	-- Color setting is global.
+	love.graphics.setColor(0,0,0)
+	love.graphics.print("Hello World", 400, 400)
+
+	love.graphics.setColor(255,255,255)
+	love.graphics.draw(blueTile, imgx, imgy)
+	love.graphics.draw(redTile, 250,100)
+
+	
+end
+
+function love.mousepressed(x,y,button)
+	print("mouse pressed")
+	print(button)
+	if button == 1 then
+		imgx, imgy = x,y
+	end
+end
+
+function love.mousereleased(x,y,button)
+	if (button == '1') then
+		-- Do something
+	end
+end
+
+function love.keypressed(key)
+	if key == 'b' then
+		-- the b key was pressed
+	end
+end
+
+function love.keyreleased(key)
+	-- The key was released
+end
+
+function love.focus(f)
+	if not f then
+		print("Lost focus")
+	else
+		print("Gained focus")
+	end
+end
+
+function love.quit()
+	-- Before quits runs this:
+end
+
+
+
