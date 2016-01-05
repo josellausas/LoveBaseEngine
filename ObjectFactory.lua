c4cc1708e19b707febd37ffc5e7b3936c408c3e7
josellausas <jose@josellausas.com>
2016-01-05 03:40:23 -0600

    Nice and beautyful with comments.

--- ObjectFactory.lua
+++ ObjectFactory.lua
@@ -1,24 +1,31 @@
 --[[ ObjectFactory.lua ]]
 
+-- Offsets the rotation so it looks good.
 local rotate90 = math.rad(90)
 
 
---[[ FUNC TABLES??? ]]
+--[[ Updates the lifetime of an object ]]
 local update_lifeTime = function(self, dt)
 	self.lifeTime = self.lifeTime + dt
 end
 
+
+
+--[[Moves the object in the direction it is facing]]
 local update_movement = function(self, dt)
 	-- Move towards the forwards vector at the given speed.
 	local fwdVector = {
 		x = math.cos(self.heading),
 		y = math.sin(self.heading)
 	}
-
+	-- Update the position with deltas.
 	self.x = self.x + (fwdVector.x * self.speed * dt)
 	self.y = self.y + (fwdVector.y * dt * self.speed)
 end
 
+
+
+--[[Looks at the target if he have one.]]
 local update_intelligence = function(self, dt)
 	-- Just looks at the objective for now
 	if not (self.target == nil) then
@@ -26,10 +33,16 @@ local update_intelligence = function(self, dt)
 	end
 end
 
+
+
+--[[Draws static things, no scale, no offsets, etc.]]
 local draw_basic = function(self)
 	love.graphics.draw(self.image, self.x, self.y)
 end
 
+
+
+--[[ Draws the thing in the center, scaled and rotated in the correct direction ]]
 local draw_advanced = function(self)
 	love.graphics.draw(self.image, self.x, self.y, self.heading + rotate90, self.scale.x, self.scale.y, self.spec.offX, self.spec.offY)
 end
@@ -37,7 +50,6 @@ end
 
 
 --[[  RenderObject
-
 	The most basic render object.
 	Note: This is not offset to the center.
 	This is used for things that do not move.
@@ -62,6 +74,7 @@ local RenderObject =
 }
 
 
+--[[ An image that can move]]
 local MovingObject = 
 {
 	new = function(self, renderImage)
@@ -109,26 +122,25 @@ local MovingObject =
 	end
 }
 
+--[[ A Moving object that seems to be intelligent ]]
 local IntelligentObject = 
 {
+	-- Our constructor method.
 	new = function(self, renderImage, target)
+		-- The new instance we are creating
 		local ai = MovingObject:new(renderImage)
-
+		-- Sets our target
 		ai.target = target
 
-		ai.setTarget = function(self, target)
-			self.target = target
-		end
-
+		--[[ Sets the heading in the direciton of the target ]]
 		ai.lookAt = function(self, coord)
-			--[[Look at behavior]]
-
-			-- Get the targets pos
+			-- Get the targets position.
 			local targetX = coord.x
 			local targetY = coord.y
 			-- Get a vector from us to the target (distVector)
 			local distX = targetX - self.x
 			local distY = targetY - self.y
+			-- Figure out our forward vector (has to be normalized)
 			local fwd = {
 				x = distX,
 				y = distY
@@ -139,13 +151,12 @@ local IntelligentObject =
 				fwd.x = distX / length
 				fwd.y = distY / length
 			end
-
+			-- Sets the heading to our forward vector.
 			self:setHeading(fwd.x, fwd.y)
 		end
 
 		-- Polymorphism
 		ai.update = function(self, dt)
-			-- Call the parents update
 			update_lifeTime(self, dt)
 			update_movement(self, dt)
 			update_intelligence(self, dt)
@@ -155,10 +166,15 @@ local IntelligentObject =
 	end,
 }
 
+
+--[[ The main interface. Is in charge of creating and managing all objects. Needs to be updated and drawn. ]]
 local ObjectFactory = 
 {
+	--[[ Stores all the objects created with this Factory. 
+	This can be used for memory clean ups.]]
 	allObjects = {},
 
+	--[[ Creates a new Artificial Intelligence actor that has a target. ]]
 	newAI = function(self, img, posX, posY, target)
 		local inst = IntelligentObject:new(img,target)
 		inst.x = posX
@@ -168,6 +184,7 @@ local ObjectFactory =
 		return inst
 	end,
 
+	--[[ Creates a new static render object. These objects are aligned to the top-left (0,0) ]]
 	newStatic = function(self, img, posX, posY)
 		local inst = RenderObject:new(img)
 		inst.x = posX
@@ -176,6 +193,7 @@ local ObjectFactory =
 		return inst
 	end,
 
+	--[[ Creates a RenderObject that has the heading and speed properties. (it moves)]]
 	new = function(self, img, posX, posY)
 		local instance = MovingObject:new(img)
 		instance.x = posX
@@ -185,12 +203,14 @@ local ObjectFactory =
 		return instance
 	end,
 
+	--[[ Updates all RenderObjects ]]
 	update = function(self, dt)
 		for k,v in pairs(self.allObjects) do
 			v:update(dt)
 		end
 	end,
 
+	--[[ Draws all RenderObjects ]]
 	draw = function(self)
 		for k,v in pairs(self.allObjects) do
 			v:draw()
