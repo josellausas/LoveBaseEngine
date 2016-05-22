48355b23050a081d8b867a7f8ef5f9635260b43d
josellausas <jose@josellausas.com>
2016-05-22 01:25:22 -0500

    Ok

--- LLBase/AI/Behaviors.lua
+++ LLBase/AI/Behaviors.lua
@@ -1,22 +1,23 @@
 --[[Behavior state machines]]
-local noBehavior = {
-	onEnter = function(agent)
-		print("starting to chill")
-		agent.speed = 0
-	end,
-	onUpdate = function(agent, dt)
-		agent.heading = agent.heading + dt
-	end,
-	onExit = function(agent)
-		print("Exiting idle state")
-	end
-}
+
+local noBehavior = {}
+function noBehavior:onEnter()
+	print("starting to chill")
+	self.speed = 0
+end
+
+function noBehavior:onUpdate(dt)
+	self.heading = self.heading + dt
+end
+
+function noBehavior:onExit()
+	print("No longer chilling")
+end
 
 
 local seekBehavior = {
 	onEnter = function(agent)
 		print("Agent is now seeking target at: ")
-
 	end,
 	onUpdate = function(agent, dt)
 		if not (agent.target == nil) then
@@ -31,6 +32,7 @@ local seekBehavior = {
 		print("Agent is no longer seeking target")
 	end
 }
+
 
 local wanderBehavior = {
 	onEnter = function(agent)
