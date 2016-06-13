ab2d4af5b1c73bc0276eb9f79cd4dc190771171a
josellausas <jose@josellausas.com>
2016-06-13 15:49:56 -0500

    Ok

--- conf.lua
+++ conf.lua
@@ -1,15 +1,15 @@
 -- Love Config file
 function love.conf(t)
-	t.identity = "baseEngine_0"
-	t.modules.joystick = false
-	t.window.title = "BaseEngine by Llau"
-	t.window.icon = nil -- Filepath to an image to use as icon
-	t.window.width = 1024
-	t.window.height = 768
-	t.window.resizable = true
-	t.window.minWidth = 800
-	t.window.minHeight = 600
-	t.window.msaa = 0 -- Number of samples to use
-	t.window.display = 1 -- The index of the monitor to use
+	t.identity 			= "baseEngine_0"
+	t.modules.joystick 	= false
+	t.window.title 		= "BaseEngine by Llau"
+	t.window.icon 		= nil -- Filepath to an image to use as icon
+	t.window.width 		= 1024
+	t.window.height 	= 768
+	t.window.resizable 	= true
+	t.window.minWidth 	= 800
+	t.window.minHeight 	= 600
+	t.window.msaa 		= 0 -- Number of samples to use
+	t.window.display 	= 1 -- The index of the monitor to use
 end
 
---
+++ thread01.lua
@@ -0,0 +1,36 @@
+require("love.timer")
+require("love.image")
+
+--To perform thread operations, you have to find yourself
+local this_thread = love.thread.getThread() --or love._curthread
+
+function generateImage()
+  math.randomseed(love.timer.getTime())
+  function pixelFunction(x, y, r, g, b, a)
+    r = math.random(10) + b + x % 10
+    g = math.random(10) + r + y % 10
+    b = math.random(10) + a + x*y % 20
+    a = math.random(10) + g + x+y % 15
+    return math.ceil(r), math.ceil(g), math.ceil(b), math.ceil(a)
+  end
+  local image = love.image.newImageData(512,512)
+  local response = nil
+  --The thread will never leave this loop until quit recieves true
+  while true do
+    image:mapPixel(pixelFunction)
+    --send the updated image back to the main process.
+    this_thread:send("progress", image)
+    if this_thread:receive("quit") then
+      --leave the function and finish the thread execution
+      return
+    end
+  end
+end
+
+--This waits for the main process to send a signal to start
+this_thread:demand("run")
+generateImage()
+
+
+--When this part is reached the thread terminates
+return thread01
